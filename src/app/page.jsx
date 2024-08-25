a46dcd4ec47a1f4a0f43f65d3d20800e8abb2ee0
NurBlessedProj <blessednur67@gmail.com>
2024-08-25 17:30:54 +0100

    pages implemented

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import NaviationBar from "@/components/NavigationBar/NaviationBar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <NaviationBar/>
+    </>
+  );
+}
---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './HomePage.module.css'
+
+function HomePage() {
+  return (
+    <div>HomePage</div>
+  )
+}
+
+export default HomePage
