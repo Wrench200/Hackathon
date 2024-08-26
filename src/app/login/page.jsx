df56e0653778f7e8e34e1166930f9ead7be7bdc3
Daltonk200 <mbengbright3@gmail.com>
2024-08-26 02:50:03 +0100

    login and register ui done

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import Image from 'next/image'
 
 function page() {
   return (
-    <div>page</div>
+    <section className='flex flex-col items-center justify-center h-screen'>
+      <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+    <form class="space-y-6" action="#">
+        <h5 class="text-xl font-medium text-gray-900 dark:text-white">Wecome back</h5>
+        <div>
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
+        </div>
+        <div>
+            <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+        </div>
+        <div className="flex items-start">
+            <div className="flex items-start">
+                <div className="flex items-center h-5">
+                    <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+                </div>
+                <label for="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+            </div>
+            <a href="#" className="ms-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
+        </div>
+        <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
+        <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+            Not registered? <a href="register" className="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
+        </div>
+        <button className="flex justify-center items-center bg-white rounded-md py-1.5 px-2.5 w-full mt-4 border ">
+          <Image 
+            src="/Google2-removebg-preview.png"
+            className="w-10"
+            width={100}
+            height={100}
+            alt="Google Logo"
+          />
+          <p className="text-black">continue with google</p>
+        </button>
+    </form>
+</div>
+    </section>
+
   )
 }
 
